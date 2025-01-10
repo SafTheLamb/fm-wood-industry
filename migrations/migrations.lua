d489722ba3a32050e28225afe6d85b88a124a65b
sapphiriza <sapphiriza@gmail.com>
2025-01-09 17:48:36 -0800

    Version 1.3.2: Fix migrations.lua

--- migrations/migrations.lua
+++ migrations/migrations.lua
@@ -1,15 +1,16 @@
 for _,force in pairs(game.forces) do
   local technologies = force.technologies
   local recipes = force.recipes
+  local mods = script.active_mods
 
   recipes["electric-kiln"].enabled = technologies["advanced-material-processing-2"].researched
-  if script.active_mods["space-age"] then
+  if mods["space-age"] then
     local carbon_technology = settings.startup["wood-industry-carbon-military"].value and "sulfur-processing" or "tungsten-carbide"
     recipes["carbon"].enabled = technologies[carbon_technology].researched
 
     recipes["reactivated-charcoal"].enabled = technologies["space-platform"].researched
 
-    local astroponics_technology = script.active_mods["astroponics"] and "astroponics" or "space-platform-thruster"
+    local astroponics_technology = mods["astroponics"] and "astroponics" or "space-platform-thruster"
     recipes["synthetic-resin"].enabled = technologies[astroponics_technology].researched
   end
 
