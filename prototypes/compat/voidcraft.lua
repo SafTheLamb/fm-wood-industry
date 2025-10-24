dfe1356c1b6fa9d538b36240a9758093153cb45a
SafTheLamb <safthelamb@gmail.com>
2025-10-24 16:30:02 -0700

    Add fluxize and coherize recipe variants for Voidcraft

--- prototypes/compat/voidcraft.lua
+++ prototypes/compat/voidcraft.lua
@@ -1,17 +1,23 @@
 if mods["Voidcraft"] then
-  local vcc = require("__Voidcraft__.prototypes.costs")
-  
-  local charcoal_bad_product = mods["space-age"] and "spoilage" or "carbon"
-  
-  VOIDCRAFT.create_auto_groups({{"voidcraft-wood", "vccw"}})
+	local vcc = require("__Voidcraft__.prototypes.costs")
 
-  data:extend({
-    VOIDCRAFT.voidcraft_recipe({prefix="__wood-industry__", subgroup="voidcraft-wood", amt2=2}, {"charcoal", "woodchips", charcoal_bad_product}, "vcba", vcc.smelted_product, "s6x-voidcraft", {0.48, 0.4, 0.1})
-  })
+	local charcoal_bad_product = mods["space-age"] and "spoilage" or "carbon"
 
-  if settings.startup["wood-industry-resin"].value then
-    data:extend({
-      VOIDCRAFT.voidcraft_recipe({prefix="__wood-industry__", subgroup="voidcraft-wood"}, {"resin", "wood", "sulfur"}, "vcbb", vcc.petrochem_product, "s6x-voidcraft", {0.48, 0.4, 0.1})
-    })
-  end
+	VOIDCRAFT.create_auto_groups({{"voidcraft-wood", "vccw"}})
+
+	local vc_recipes = {}
+	table.insert(vc_recipes, VOIDCRAFT.voidcraft_recipe({prefix="__wood-industry__", subgroup="voidcraft-wood", amt2=2}, {"charcoal", "woodchips", charcoal_bad_product}, "vcba", vcc.smelted_product, "s6x-voidcraft", {0.48, 0.4, 0.1}))
+	if settings.startup["wood-industry-resin"].value then
+		table.insert(vc_recipes, VOIDCRAFT.voidcraft_recipe({prefix="__wood-industry__", subgroup="voidcraft-wood"}, {"resin", "wood", "sulfur"}, "vcbb", vcc.petrochem_product, "s6x-voidcraft", {0.48, 0.4, 0.1}))
+	end
+
+	if vc_recipes ~= {} then
+		data:extend(vc_recipes)
+		for _,recipe in pairs(vc_recipes) do
+			data:extend({
+				VOIDCRAFT.coherize(recipe),
+				VOIDCRAFT.fluxize(recipe)
+			})
+		end
+	end
 end
